318e9377f2001c84f2e55d0f6a2d99782a655cd7
kscjje <kscjje@naver.com>
2024-01-13 19:02:28 +0900

    첫번째 저장

---
+++ src/main/java/com/simplesoft/SimplesoftApplication.java
@@ -0,0 +1,13 @@
+package com.simplesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplesoft/SimplesoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

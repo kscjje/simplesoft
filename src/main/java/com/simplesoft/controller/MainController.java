3d957de3486a21675d7f40254c43250e7f98caee
kscjj <kscjj@192.168.75.91>
2024-03-06 23:39:36 +0900

    달력 구현 중

--- src/main/java/com/simplesoft/controller/MainController.java
+++ src/main/java/com/simplesoft/controller/MainController.java
@@ -2,7 +2,7 @@ package com.simplesoft.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.simplesoft.member.service.MemberService;
 import com.simplesoft.member.service.MemberVO;
@@ -13,14 +13,19 @@ public class MainController {
 	@Autowired
 	MemberService memberService;
 	
-	@RequestMapping("/main")
+	@GetMapping("/main")
 	public String main() {
-		MemberVO vo = new MemberVO();
-		vo.setUserId("user");
-		vo.setPwd("1234");
-		vo.setGender("M");
-		vo.setMemNm("홍길동");
-		int a = memberService.insertMember(vo);
-		return "main";
+//		MemberVO vo = new MemberVO();
+//		vo.setUserId("user");
+//		vo.setPwd("1234");
+//		vo.setGender("M");
+//		vo.setMemNm("홍길동");
+//		int a = memberService.insertMember(vo);
+		return "/main";
+	}
+	
+	@GetMapping("/order")
+	public String order() {
+		return "/order/list";
 	}
 }

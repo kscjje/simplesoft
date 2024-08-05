ef3a61e985b883aafcf141605bf39dbf607ea215
kscjj <kscjj@192.168.75.91>
2024-08-05 18:00:58 +0900

    .

--- src/main/java/com/simplesoft/controller/HomeController.java
+++ src/main/java/com/simplesoft/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.simplesoft.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.simplesoft.util.GlobalVariable;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * @author kscjje
@@ -16,4 +22,35 @@ public class HomeController {
 	public String home() {
 		return "main";
 	}
+	@PostMapping({ "/hint" , "hint" })
+	public String hint(HttpServletRequest request, Model model) {
+		
+		String hintText = request.getParameter("hintText");						//힌트코드
+		String type= "";
+		if("".equals(hintText) || hintText == null) {
+			model.addAttribute("PARAM_MESSAGE", "힌트코드를 올바르게 입력해 주세요.");
+			model.addAttribute("returnUrl", "/");
+			return GlobalVariable.REDIRECT_SUBMIT;
+		} else hintText = hintText.toUpperCase();
+		
+		if("HR01".equals(hintText)) {
+			type = "01";
+		} else if("HR02".equals(hintText)) {
+			type = "02";
+		} else if("HR03".equals(hintText)) {
+			type = "03";
+		} else {
+			model.addAttribute("PARAM_MESSAGE", "힌트코드를 올바르게 입력해 주세요.");
+			model.addAttribute("returnUrl", "/");
+			return GlobalVariable.REDIRECT_SUBMIT;
+		}
+		System.out.println("type:" + type);
+		model.addAttribute("type", type);
+		return "hint";
+	}
+	@GetMapping({ "/hint" , "hint" })
+	public String gethint() {
+		return "main";
+	}
+	
 }
